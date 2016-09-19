f3d55b1dae9451ddb30c64444f7a2d085dd06a6d
Alistair Johnson <alistair.johnson@johnsonusm.com>
2016-09-19 13:51:04 +0200

    Remove local Tut

--- scripts/publishLocalScoverage.sh
+++ scripts/publishLocalScoverage.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 
 cd "${TMPDIR:-/tmp}"
 git clone -b tl-2-12 "https://github.com/BennyHill/scalac-scoverage-plugin.git"
-( cd scalac-scoverage-plugin && sbt -sbt-version 0.13.13-M1 ++2.11.8 publishLocal ++2.11.8 publishLocal ++2.12.0-RC1 publishLocal )
+( cd scalac-scoverage-plugin && sbt -sbt-version 0.13.13-M1 ++$TRAVIS_SCALA_VERSION publishLocal )
 rm -rf  scalac-scoverage-plugin
 
