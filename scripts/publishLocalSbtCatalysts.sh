dc63a29fe85d1dfd2905814b2ed497e611f7eda2
Alistair Johnson <alistair.johnson@johnsonusm.com>
2018-02-27 19:21:20 +0100

    Fix publishLocalSbtCatalysts.sh

--- scripts/publishLocalSbtCatalysts.sh
+++ scripts/publishLocalSbtCatalysts.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 
 cd "${TMPDIR:-/tmp}"
 git clone -b topic/2.13-M3 "https://github.com/BennyHill/sbt-catalysts.git"
-( cd sbt-catalysts && sbt -sbt-version 0.13.6 publishLocal)
+( cd sbt-catalysts && sbt publishLocal)
 rm -rf  sbt-catalysts
 
